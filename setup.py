aa5be454943696300739ff5a97bd83a8b924ec92
Nobel <jeremy.lebon@vives.be>
2023-03-11 18:41:27 +0000

    view_sensors.launch added

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'robot_sim_sensors'
 
@@ -10,6 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf/basic_sensors'), glob('urdf/basic_sensors/*.xacro')),
+        (os.path.join('share', package_name, 'urdf/brand_sensors'), glob('urdf/brand_sensors/*.xacro')),
+        (os.path.join('share', package_name, 'urdf/'), glob('urdf/*.xacro')),
+                
     ],
     install_requires=['setuptools'],
     zip_safe=True,
